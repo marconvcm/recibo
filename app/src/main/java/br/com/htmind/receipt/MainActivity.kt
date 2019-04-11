91826d5710291b3a990bf819a2bea9a73c6db994
Marcos <marconvcm@gmail.com>
2019-04-11 09:44:45 -0300

    Layout and first commit

--- app/src/main/java/br/com/htmind/receipt/MainActivity.kt
+++ app/src/main/java/br/com/htmind/receipt/MainActivity.kt
@@ -1,21 +1,46 @@
 package br.com.htmind.receipt
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.Menu
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import br.com.htmind.receipt.databinding.ActivityMainBinding
+import br.com.htmind.receipt.databinding.ActivityMainBindingImpl
 import br.com.htmind.receipt.vm.ReceiptViewModel
+import kotlinx.android.synthetic.main.activity_main.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    val receiptViewModel by viewModel<ReceiptViewModel>()
+    val payerEntries by lazy { resources.getStringArray(R.array.casas) }
+
+    val model by viewModel<ReceiptViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        title = getString(br.com.htmind.receipt.R.string.recibo_activity_title)
+        binding.model = model
+        binding.lifecycleOwner = this
+        binding.editTextPayer.setOnClickListener { view ->
 
+            val editText = view as EditText
 
+            AlertDialog.Builder(this@MainActivity).apply {
+                setTitle(editText.hint)
+                setItems(payerEntries) { dialog, which ->
+                    model.payer.postValue(payerEntries[which])
+                }
+                setNegativeButton("Cancel") { dialog, which -> }
+            }.apply {
+                create()
+                show()
+            }
+        }
     }
 }
--- app/src/main/java/br/com/htmind/receipt/vm/ReceiptViewModel.kt
+++ app/src/main/java/br/com/htmind/receipt/vm/ReceiptViewModel.kt
@@ -1,8 +1,16 @@
 package br.com.htmind.receipt.vm
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ReceiptViewModel : ViewModel() {
+class ReceiptViewModel: ViewModel() {
 
-    
+    var payer = MutableLiveData<String>()
+    var amount = MutableLiveData<String>()
+    var refers = MutableLiveData<String>()
+
+    fun say() {
+        Log.d("MARCOS", amount.value);
+    }
 }
